9ed952bdc3fd0b15521a896e5f1dd841f1a74809
victoriamurigi <vickymurigi001@gmail.com>
2024-08-20 12:07:01 +0300

    setting up client-side routing with createbrowserrouter and routerprovider

--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,13 +1,36 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import NavBar from "../components/NavBar";
 
 function Movie() {
+  const [movie ,setMovie] = useState(null)
+  const { id } = useParams()
+
+  useEffect(() => {
+     fetch(`http://localhost:4000/movies/${id}`)
+       .then((res) => res.json())
+       .then((data) => {
+         setMovie(data);
+       })
+       .catch((err) => console.error(err));
+  }, [id])
+
+  if(!movie)return <h2>Loading...</h2>
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Movie info here! */}
+        <h1>{movie.title}</h1>
+        <p>{movie.time}</p>
+        {movie.genres.map((genre) => {
+        <span 
+        key={genre}
+        >{genre}</span>;
+        })}
+
       </main>
     </>
   );
